f5cbfbe5408abc8c51b3ede7f2258fd50187f332
Alex Tavares <alex1302@live.com>
2019-06-21 15:50:36 -0300

    fixing identation

--- Ch. 1/Section 2/1.2.1/Points.java
+++ Ch. 1/Section 2/1.2.1/Points.java
@@ -5,12 +5,12 @@ import edu.princeton.cs.algs4.StdRandom;
 
 class Points {
 
-    static int N = 10; //Number of points pair
+    static int N = 10; //Number of points
 
-    public static void findShortestDist(Point2D[] points){
+    public static void findShortestDist(Point2D[] points) {
 
-        Point2D point1 = new Point2D(0,0);
-        Point2D point2 = new Point2D(0,0);
+        Point2D point1 = new Point2D(0, 0);
+        Point2D point2 = new Point2D(0, 0);
 
         double distance;
         double shortestDistance = Double.MAX_VALUE;
@@ -18,12 +18,12 @@ class Points {
 
         int count = 0;
 
-        for(int i=0; i<N; i++){
-            for(int j=1+c;j<N;j++){
+        for (int i = 0; i < N; i++) {
+            for (int j = 1 + c; j < N; j++) {
 
                 distance = points[i].distanceTo(points[j]);
 
-                if(distance < shortestDistance){
+                if (distance < shortestDistance) {
                     shortestDistance = distance;
                     point1 = points[i];
                     point2 = points[j];
@@ -39,19 +39,19 @@ class Points {
         StdDraw.setPenRadius(.002);
         StdDraw.line(point1.x(), point1.y(), point2.x(), point2.y());
 
-        System.out.println("count: "+count);
+        System.out.println("count: " + count);
 
     }
 
-     
+
 
     public static void main(String[] args) {
         //StdDraw.setCanvasSize(1024, 1024);
 
-        
+
 
         StdDraw.setCanvasSize(500, 500);
-		StdDraw.setPenRadius(.015);
+        StdDraw.setPenRadius(.015);
         StdDraw.setXscale(0, 1);
         StdDraw.setYscale(0, 1);
         StdDraw.setPenColor(StdDraw.BLUE);
@@ -61,15 +61,15 @@ class Points {
         for (int i = 0; i < N; i++) {
 
             double xCorrdinate = StdRandom.uniform();
-			double yCoordinate = StdRandom.uniform();
-            
+            double yCoordinate = StdRandom.uniform();
+
             points[i] = new Point2D(xCorrdinate, yCoordinate);
-        
+
             StdDraw.point(points[i].x(), points[i].y());
 
         }
 
         findShortestDist(points);
-        
+
     }
 }
